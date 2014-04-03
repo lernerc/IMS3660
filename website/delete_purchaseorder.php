64070d2eb0250a70aea2b8424bd763eef6a48eb9
Camara Lerner <camara.lerner@gmail.com>
2014-04-02 19:17:31 -0600

    added delete for purchase order

---
+++ website/delete_purchaseorder.php
@@ -0,0 +1,46 @@
+<html>
+<head><title>Inventory Management System</title></head>
+<body>
+
+
+
+<?php
+if(isset($_COOKIE["username"])){
+
+   echo "<form action=\"deletepurchaseorder.php\" method=post>";
+
+   $username = $_COOKIE["username"];
+   $password = $_COOKIE["password"];
+
+   $conn = mysql_connect("cronus.cs.uleth.ca",$username,$password) or die(mysql_error());
+   mysql_select_db($username,$conn);
+   $sql = "select orderID from PURCHASE_ORDER";
+   $result = mysql_query($sql,$conn);
+   if(mysql_num_rows($result) != 0)
+   {
+      echo "Order ID: <select name=\"oid\">";
+
+      while($val = mysql_fetch_row($result))
+      {
+	 echo "<option value=$val[0]>$val[0]</option>";
+
+      }
+      echo "</select>";
+      echo "<input type=submit name=\"submit\" value=\"Delete Purchase Order\">";
+   }
+   else
+   {
+      echo "<p>No Purchase Orders! </p>";
+   }
+
+   echo "</form>";
+} else {
+   echo "<h3>You are not logged in!</h3><p> <a href=\"login.php\">Login First</a></p>";
+
+}
+?>
+
+
+
+</body>
+</html>
