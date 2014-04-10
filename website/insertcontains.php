10c8fb9d0ff07ba9e944409b6e75d1cc24fe63cf
Camara Lerner <camara.lerner@gmail.com>
2014-04-10 17:58:35 -0600

    added quantity check before being able to add items into a cart

--- website/insertcontains.php
+++ website/insertcontains.php
@@ -9,29 +9,33 @@ if (isset($_COOKIE["username"])) {
    mysql_select_db($username,$conn) or die(mysql_error());
    $item = explode(',', $_POST[item]);
 
-   $lookUp = "select sum(quantity) from LOCATED where productNum=$item[0] and barcode=$item[1]";
-   echo $lookUp;
-   $table = mysql_query($lookup, $conn);
+   $lookUp = "select SUM(quantity) from LOCATED where productNum=$item[0] and barcode=$item[1]";
+   $taken = "select sum(quantity) from CONTAINS where productNum=$item[0] and barcode=$item[1]";
+   $table = mysql_query($lookUp, $conn);
+   $takenTable = mysql_query($taken, $conn);
    if(mysql_num_rows($table) == 0) {
       echo "<h3>There are no $item[2] available</h3>";
    } else {
-       $num = mysql_fetch_row($table);
-       echo $num[0];
-       if($num[0] < $_POST[num]) {
-	  echo "<h3>There are only $num[0] of $item[2] available!</h3>";
-       } else {   
-	  $sql = "insert into CONTAINS values ('$_POST[cid]','$item[0]','$item[1]','$_POST[num]')";
-	  if(mysql_query($sql,$conn)) {
-	     echo "<h3> Item added into Cart!</h3>";
-	  } else {
-	     $err = mysql_errno();
-	     if($err == 1062) {
-		echo "<h3>Item $item[2] is already in Cart with ID $_POST[cid]!</h3>";
-	     } else {
-		echo "error number $err";
-	     }
-	  }
-       }
+      $num = mysql_fetch_row($table);
+      if(mysql_num_rows($takenTable) != 0) {
+	 $t = mysql_fetch_row($takenTable);
+	 $num[0] -= $t[0];
+      }
+      if($num[0] < $_POST[num]) {
+	 echo "<h3>There are only $num[0] of $item[2] available!</h3>";
+      } else {   
+	 $sql = "insert into CONTAINS values ('$_POST[cid]','$item[0]','$item[1]','$_POST[num]')";
+	 if(mysql_query($sql,$conn)) {
+	    echo "<h3> Item added into Cart!</h3>";
+	 } else {
+	    $err = mysql_errno();
+	    if($err == 1062) {
+	       echo "<h3>Item $item[2] is already in Cart with ID $_POST[cid]!</h3>";
+	    } else {
+	       echo "error number $err";
+	    }
+	 }
+      }
    }
    echo "<a href=\"main.php\">Return</a> to Home Page.";
 } else {
