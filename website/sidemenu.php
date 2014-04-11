f11fd0055dfef12692d74cf608bdc7e13cf8542c
cpinter10 <cpinter10@gmail.com>
2014-04-10 18:33:36 -0600

    side menu checks for manager or employee

--- website/sidemenu.php
+++ website/sidemenu.php
@@ -3,18 +3,25 @@ if(isset($_COOKIE["username"])){
    $username = $_COOKIE["username"];
    $password = $_COOKIE["password"];
    if(isset($_COOKIE["subusername"])){
-   $subusername = $_COOKIE["subusername"];
-   
-   $conn = mysql_connect("cronus.cs.uleth.ca",$username,$password) or die(mysql_error());
-   mysql_select_db($username,$conn);
-
-   echo "<table>
+      $subusername = $_COOKIE["subusername"];
+      
+      $conn = mysql_connect("cronus.cs.uleth.ca",$username,$password) or die(mysql_error());
+      mysql_select_db($username,$conn);
+      
+      echo "<table>
 <tr>
 <td>
- <ul>
-          <li><a href=\"my_carts.php\">Carts</a></li>
-          <li><a href=\"show_stores.php\">Stores</a></li>
-          <li><a href=\"delete_user.php\">Delete User</a></li>
+ <ul>";
+      echo "<li><a href=\"my_carts.php\">Carts</a></li>
+          <li><a href=\"show_stores.php\">Stores</a></li>";
+      $managerCheck = "select * from MANAGER where username='$subusername'";
+      $employeeCheck = "select * from EMPLOYEE where username='$subusername'";
+      $result2 = mysql_query($managerCheck, $conn);
+      $result3 = mysql_query($employeeCheck, $conn);
+      if(mysql_num_rows($result2) != 0)
+      {
+	 $manager = TRUE;
+	 echo "<li><a href=\"delete_user.php\">Delete User</a></li>
           <li><a href=\"insert_customer.php\">Insert Customer</a></li>
           <li><a href=\"delete_customer.php\">Delete Customer</a></li>
           <li><a href=\"insert_employee.php\">Insert Employee</a></li>
@@ -38,13 +45,20 @@ if(isset($_COOKIE["username"])){
           <li><a href=\"insert_process.php\">Insert a Cart into an Order</a></li>
           <li><a href=\"delete_process.php\">Delete a Cart from an Order</a></li>
           <li><a href=\"insert_located.php\">Insert Located</a></li>
-          <li><a href=\"delete_located.php\">Delete Located</a></li>
-
-       </ul>
+          <li><a href=\"delete_located.php\">Delete Located</a></li>";
+      }
+      else if(mysql_num_rows($result3) != 0)
+      {
+	 $employee = TRUE;
+    echo "<li><a href=\"insert_located.php\">Insert Located</a></li>
+          <li><a href=\"delete_located.php\">Delete Located</a></li>";
+      }
+      else
+     echo "</ul>
 </td>
 </tr>
 </table>";
-   
+      
    }
    else
    {
@@ -52,6 +66,6 @@ if(isset($_COOKIE["username"])){
    }
 } else {
    echo "<h3>You are not logged in to a database!</h3><p> <a href=\"login.php\">Login First</a></p>";
-
+   
 }
 ?>
