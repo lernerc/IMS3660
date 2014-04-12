3650ea7ea29e461a4996cd1e39acb3abd3ff1922
David Fox <dafoxco@gmail.com>
2014-04-11 19:45:03 -0600

    added content on aboutus.php page

--- website/aboutus.php
+++ website/aboutus.php
@@ -7,6 +7,12 @@ echo "</td>";
 
 echo '<td valign="top">';
 echo '<h2>About Us</h2>';
+
+
+echo '<div><p> This Inventory Management System was created for the group project portion, of the course Introduction to Databases at the Univeristy of Lethbridge spring 2014</p>';
+echo 'Group members consist of:';
+echo '<br>Camara Lerner<br>Chris pinter<br>David Fox';
+echo '</div>';
 echo '</td>';
 
 include 'footer.php';
