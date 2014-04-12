e2469cd95bb6be4138cd36afcb4f2c3f4080ff08
Camara Lerner <camara.lerner@gmail.com>
2014-04-12 15:36:39 -0600

    fixed prices in show_orders

--- website/show_orders.php
+++ website/show_orders.php
@@ -26,22 +26,23 @@ if($manager == TRUE)
    $sql = "select * from PURCHASE_ORDER where createdBy='$subusername'";
    $result = mysql_query($sql,$conn);
    echo "<table><tr>";
-   echo "<th>Order ID</th>";
-   echo "<th>Date</th>";
-   echo "<th>Total Price</th>";
-   echo "<th>Creator</th>";
-   echo "<th>Manager ID</th>";
+   echo "<th valign='top'>Order ID</th>";
+   echo "<th valign='top'>Date</th>";
+   echo "<th valign='top'>Total Price</th>";
+   echo "<th valign='top'>Creator</th>";
+   echo "<th valign='top'>Manager ID</th>";
    echo "</tr>";
    while($val = mysql_fetch_row($result))
    {
       echo "<tr>";
-      echo "<td>$val[0]</td>";
-      echo "<td>$val[1]</td>";
-      echo "<td>$val[2]</td>";
-      echo "<td>$val[3]</td>";
-      echo "<td>$val[4]</td>";
+      echo "<td valign='top'>$val[0]</td>";
+      echo "<td valign='top'>$val[1]</td>";
+      $output=number_format($val[2]/100, 2,'.','');
+      echo "<td valign='top' align='right'>$$output</td>";
+      echo "<td valign='top'>$val[3]</td>";
+      echo "<td valign='top'>$val[4]</td>";
       echo "<form action=\"update_order.php\" method=post>";
-      echo "<td><input type=\"hidden\" name=\"id\" value=\"$val[0]\"><input type=submit name=\"submit\" value=\"Edit Order\"></td>";
+      echo "<td valign='top'><input type=\"hidden\" name=\"id\" value=\"$val[0]\"><input type=submit name=\"submit\" value=\"Edit Order\"></td>";
       echo "</form>";
       echo "</tr>";
    }
@@ -55,22 +56,23 @@ if($manager == TRUE || $employee == TRUE)
    {
       echo "<h3>Orders</h3>";
       echo "<table><tr>";
-      echo "<th>Order ID</th>";
-      echo "<th>Date</th>";
-      echo "<th>Total Price</th>";
-      echo "<th>Creator</th>";
-      echo "<th>Manager ID</th>";
+      echo "<th valign='top'>Order ID</th>";
+      echo "<th valign='top'>Date</th>";
+      echo "<th valign='top'>Total Price</th>";
+      echo "<th valign='top'>Creator</th>";
+      echo "<th valign='top'>Manager ID</th>";
       echo "</tr>";
       while($val = mysql_fetch_row($result))
       {
 	 echo "<tr>";
-	 echo "<td>$val[0]</td>";
-	 echo "<td>$val[1]</td>";
-	 echo "<td>$val[2]</td>";
-	 echo "<td>$val[3]</td>";
-	 echo "<td>$val[4]</td>";
+	 echo "<td valign='top'>$val[0]</td>";
+	 echo "<td valign='top'>$val[1]</td>";
+	 $output=number_format($val[2]/100, 2, '.', '');
+	 echo "<td valign='top' align='right'>$$output</td>";
+	 echo "<td valign='top'>$val[3]</td>";
+	 echo "<td valign='top'>$val[4]</td>";
 	 echo "<form action=\"view_order.php\" method=post>";
-	 echo "<td><input type=\"hidden\" name=\"id\" value=\"$val[0]\"><input type=submit name=\"submit\" value=\"View Order\"></td>";
+	 echo "<td valign='top'><input type=\"hidden\" name=\"id\" value=\"$val[0]\"><input type=submit name=\"submit\" value=\"View Order\"></td>";
 	 echo "</form>";
 	 echo "</tr>";
       }
