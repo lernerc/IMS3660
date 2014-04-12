435b28228d3be1d78b30e80b33aacb4e7c75f43b
Camara Lerner <camara.lerner@gmail.com>
2014-04-12 15:27:02 -0600

    fixed price in view_cart

--- website/view_cart.php
+++ website/view_cart.php
@@ -12,35 +12,39 @@ while($val = mysql_fetch_row($result))
    echo "Cart ID: $val[0]<br>";
    echo "<input type=\"hidden\" name=\"id\" value=\"$val[0]\">";
    echo "Created by: $val[1]<br>";
-   echo "Total Price: $val[2]<br>";
+   $output=number_format($val[2]/100, 2,'.', '');
+   echo "Total Price: $$output<br>";
    echo "<h3>Item List</h3>";
    echo "<table><tr>";
-   echo "<th>Product Number</th>";
-   echo "<th>Barcode</th>";
-   echo "<th>Name</th>";
-   echo "<th>Description</th>";
-   echo "<th>Price</th>";
+   echo "<th valign='top'>Product Number</th>";
+   echo "<th valign='top'>Barcode</th>";
+   echo "<th valign='top'>Name</th>";
+   echo "<th valign='top'>Description</th>";
+   echo "<th valign='top'>Price</th>";
    if($employee == TRUE || $manager == TRUE)
-      echo "<th>Purchase Price</th>";
-   echo "<th>Quantity</th>";
+      echo "<th valign='top'>Purchase Price</th>";
+   echo "<th valign='top'>Quantity</th>";
    echo "</tr>";
    $sql2 = "select * from CONTAINS where cartID='$val[0]'";
    $result2 = mysql_query($sql2,$conn);
    while($val2 = mysql_fetch_row($result2))
    {
       echo "<tr>";
-      echo "<td>$val2[1]</td>";
-      echo "<td>$val2[2]</td>";
+      echo "<td valign='top'>$val2[1]</td>";
+      echo "<td valign='top'>$val2[2]</td>";
       $sql1 = "select * from ITEMS where productNum='$val2[1]' and barcode='$val2[2]'";
       $result1 = mysql_query($sql1,$conn);
       while($val1 = mysql_fetch_row($result1))
       {
-	 echo "<td>$val1[2]</td>";
-	 echo "<td>$val1[3]</td>";
-	 echo "<td>$val1[4]</td>";
-	 if($employee == TRUE || $manager == TRUE)
-	    echo "<td>$val1[5]</td>";
-	 echo "<td>$val2[3]</td>";
+	 echo "<td valign='top'>$val1[2]</td>";
+	 echo "<td valign='top'>$val1[3]</td>";
+	 $output=number_format($val1[4]/100, 2,'.', '');
+	 echo "<td valign='top' align='right'>$$output</td>";
+	 if($employee == TRUE || $manager == TRUE) {
+	    $output=number_format($val1[5]/100, 2,'.', '');
+	    echo "<td valign='top' align='right'>$$output</td>";
+	 }
+	 echo "<td valign='top'>$val2[3]</td>";
       }
       echo "</tr>";
    }
