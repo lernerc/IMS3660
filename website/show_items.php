54abb8802ffa3c8d1f7c7082c866bdea21be9a6f
Camara Lerner <camara.lerner@gmail.com>
2014-04-12 14:42:23 -0600

    show_items.php update table to line everything up

--- website/show_items.php
+++ website/show_items.php
@@ -47,25 +47,25 @@ echo "<input type=submit name='submit' value='Search Items'> </form>";
 $sql = "select * from ITEMS";
 $result = mysql_query($sql,$conn);
 echo "<table><tr>";
-echo "<th>Product Number</th>";
-echo "<th>Barcode</th>";
-echo "<th>Name</th>";
-echo "<th>Description</th>";
-echo "<th>Price</th>";
+echo "<th valign='top'>Product Number</th>";
+echo "<th valign='top'>Barcode</th>";
+echo "<th valign='top'>Name</th>";
+echo "<th valign='top'>Description</th>";
+echo "<th valign='top'>Price</th>";
 if($employee == TRUE || $manager == TRUE)
-   echo "<th>Purchase Price</th>";
+   echo "<th valign='top'>Purchase Price</th>";
 echo "</tr>";
 while($val = mysql_fetch_row($result))
 {
    echo "<tr>";
-   echo "<td>$val[0]</td>";
+   echo "<td valign='top'>$val[0]</td>";
    $sql1 = "select * from ITEMS where productNum='$val[0]' and barcode='$val[1]'";
    $result1 = mysql_query($sql1,$conn);
    while($val1 = mysql_fetch_row($result1))
    {
-      echo "<td>$val[1]</td>";
-      echo "<td>$val1[2]</td>";
-      echo "<td>$val1[3]</td>";
+      echo "<td valign='top'>$val[1]</td>";
+      echo "<td valign='top'>$val1[2]</td>";
+      echo "<td valign='top'>$val1[3]</td>";
       $pricedollar = $val1[4]/100;echo "<td>$pricedollar</td>";
       if($employee == TRUE || $manager == TRUE)
 	 $purchdollar = $val1[5]/100;echo "<td>$purchdollar</td>";
@@ -89,7 +89,7 @@ while($val = mysql_fetch_row($result))
       }
       if($manager == TRUE)
       {
-	 echo "<td><form action=\"updateitem.php\" method=post>";
+	 echo "<td valign='top'><form action=\"updateitem.php\" method=post>";
 	 echo "<input type=\"hidden\" name=\"pNum\" value=\"$val[0]\">";
 	 echo "<input type=\"hidden\" name=\"bar\" value=\"$val[1]\">";
 	 echo "<input type=submit name=\"submit\" value=\"Update\">";
