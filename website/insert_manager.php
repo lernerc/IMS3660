ba95679843ac7b797d43d00847621a886819ee2b
cpinter10 <cpinter10@gmail.com>
2014-04-11 19:13:06 -0600

    valign fix for several pages and add to cart button removal on items page if no carts

--- website/insert_manager.php
+++ website/insert_manager.php
@@ -1,14 +1,12 @@
+<html>
+<head><title>Inventory Management System</title></head>
+<body>
+
+
 
 <?php
-include 'topmenu.php';
-echo "<table width='70%' align='center'><tr><td valign='top' width='25%'>";
-
-include 'sidemenu.php';
-echo "</td>";
-
-echo "<td valign='top'>";
 if(isset($_COOKIE["username"])){
-   echo "<h2>Insert a Manager</h2>";
+
    echo "<form action=\"insertmanager.php\" method=post>";
 
    $username = $_COOKIE["username"];
@@ -41,8 +39,9 @@ if(isset($_COOKIE["username"])){
    echo "<h3>You are not logged in!</h3><p> <a href=\"login.php\">Login First</a></p>";
 
 }
-echo '</td>';
-include 'footer.php';
 ?>
 
 
+
+</body>
+</html>
